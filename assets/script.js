ab3761851a55b8567a737f8974f399db0bab8900
ezmaa <ezinmark@gmail.com>
2022-11-04 10:45:23 -0400

    structered the page using bootstrap in html, Started function for searching for city in js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+
+// function to search for city 
+
+function currentCity() {
+
+    let apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+console.log(currentCity);
+    
+};
+
