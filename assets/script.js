1559ff05d700f419143e76f43e861fe5ec65f256
ezmaa <ezinmark@gmail.com>
2022-11-04 13:19:14 -0400

    looking at fetch issues

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,36 @@
+const searchBtn = $('#searchBtn');
+const apiKey = '8c6afd615baf70a3f5611f6679bd0674';
+// button function to search for city 
 
+// function currentCity() {
 
-// function to search for city 
+//     let apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}';
+
+//     $.ajax({
+//         url: apiUrl,
+//         method: 'GET',
+//       }).then(function (response) {
+//         console.log('Ajax Reponse \n-------------');
+//         console.log(response);
+//       });
+
+// console.log(currentCity);
+    
+// };
+
 
 function currentCity() {
 
-    let apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
-console.log(currentCity);
-    
-};
+    let apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}';
 
+    fetch(apiUrl)
+    .then(function (response){
+        return response.json();
+})
+    .then(function (data) {
+        console.log(data)
+    });
+}
+searchBtn.on('click', function() {
+    currentCity();
+});
