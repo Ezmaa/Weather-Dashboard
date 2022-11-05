dc0cade84383aaaf7e804732ac32bb9f53c56940
ezmaa <ezinmark@gmail.com>
2022-11-05 17:14:30 -0400

    rewrote js syntax and added query selectors

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,64 @@
-const searchBtn = $('#searchBtn');
+const searchBtn = document.getElementById('searchBtn');
+let userInput = document.getElementById('userInput');
+const weatherDisplay = document.getElementById('weatherDisplay');
+const fiveDayForecast = document.getElementById('fiveDayForecast');
+const searchHistory = document.getElementById('searchHistory');
+
+const lat = 42.7654;
+const lon = 71.4676;
+const today = dayjs().format('L LT');
+let city = "";
 const apiKey = '8c6afd615baf70a3f5611f6679bd0674';
 // button function to search for city 
 
-// function currentCity() {
-
-//     let apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}';
-
-//     $.ajax({
-//         url: apiUrl,
-//         method: 'GET',
-//       }).then(function (response) {
-//         console.log('Ajax Reponse \n-------------');
-//         console.log(response);
-//       });
-
-// console.log(currentCity);
-    
-// };
 
 
-function currentCity() {
 
-    let apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}';
+let citySearch = function (event) {
+    event.preventDefault();
+
+    let city = userInput.value.trim();
+
+    let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
 
     fetch(apiUrl)
     .then(function (response){
         return response.json();
 })
     .then(function (data) {
+        console.log('Users input: \n---------');
         console.log(data)
     });
-}
-searchBtn.on('click', function() {
-    currentCity();
-});
+
+
+
+    if (userInput) {
+        currentCity(userInput);
+    }
+
+
+
+};
+
+
+function currentCity() {
+
+    let lat = 
+    let lon =
+
+    let latUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    fetch(latUrl)
+    .then(function (response){
+        return response.json();
+})
+    .then(function (data) {
+        console.log('Users input: \n---------');
+        console.log(data)
+    });
+};
+
+
+
+
+searchBtn.addEventListener('click', citySearch);
