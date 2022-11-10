9b700d136ac2cc5c4a658dbc8d83f0bf662a6da7
ezmaa <ezinmark@gmail.com>
2022-11-10 11:24:20 -0500

    appended todays weather information to the html

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ const searchHistory = document.getElementById('searchHistory');
 const cityWeather = document.getElementById('cityWeather');
 
 
-const today = dayjs().format('L LT');
+const today = dayjs().format('MM-DD-YYYY');
 let city = "";
 const apiKey = '8c6afd615baf70a3f5611f6679bd0674';
 let weatherArr = [];
@@ -16,23 +16,23 @@ let weatherArr = [];
 
 // User eneters name of city and gets the coordinates for that city 
 let citySearch = function () {
-    
+
 
     let city = userInput.value.trim();
 
     let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
 
     fetch(apiUrl)
-    .then(function (coordinateResponse){
-        return coordinateResponse.json();
-})
-    .then(function (coordinatedata) {
+        .then(function (coordinateResponse) {
+            return coordinateResponse.json();
+        })
+        .then(function (coordinatedata) {
 
-        const lon = coordinatedata[0].lon;
-        const lat = coordinatedata[0].lat;
+            const lon = coordinatedata[0].lon;
+            const lat = coordinatedata[0].lat;
 
-        getCityWeather(lat, lon); 
-    });
+            getCityWeather(lat, lon);
+        });
 
 
 };
@@ -43,18 +43,18 @@ function getCityWeather(lat, lon) {
     const latUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     fetch(latUrl)
-    .then(function (response){
-        return response.json();
-})
-    .then(function (weatherData) {
-        console.log('city: \n---------');
-        console.log(weatherData);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (weatherData) {
+            console.log('city: \n---------');
+            console.log(weatherData);
 
-        displayCityWeather(weatherData);
-    });
+            displayCityWeather(weatherData);
+        });
 
-  
-    
+
+
     // cityWeather.append(weatherData.list[0].weather);
 
 };
@@ -63,35 +63,101 @@ function getCityWeather(lat, lon) {
 function displayCityWeather(weatherData) {
 
     let weatherArr = weatherData.list;
-        console.log(weatherArr);
+    console.log(weatherArr);
 
-        const todaysWeather = weatherArr[0]
-        console.log(todaysWeather);
+    const todaysWeather = weatherArr[0]
+    console.log(todaysWeather);
 
-        for(i=1; i<6; i++) {
-            console.log(weatherArr[i]);
-        }
-        const description = weatherData.list[0].weather[0].description;
-        cityWeather.append(`description: ${description}`);
-        const temp = weatherData.list[0].main.temp;
-        const wind = weatherData.list[0].wind.gust;
-        const humidity = weatherData.list[0].main.humidity;
+    for (i = 1; i < 6; i++) {
+        console.log(weatherArr[i]);
+    }
+    // const description = weatherData.list[0].weather[0].description;
+    // cityWeather.append(`description: ${description}`);
+    const temp = (Math.round(weatherData.list[0].main.temp - 273.15) * 9/5 + 32);
+    const wind = weatherData.list[0].wind.speed;
+    const humidity = weatherData.list[0].main.humidity;
+    const icon = weatherData.list[0].weather[0].icon;
+    const cityInput = weatherData.city.name;
 
-        console.log(temp);
-        console.log(wind);
-        console.log(humidity );
+    console.log(cityInput);
+    //    let todayWeather = $("<div>").addClass("card-body bg-dark border mt-3");
+    //     cityWeather.append(todayWeather);
+
+
+    //display city name
+    let cityName = document.createElement("h1");
+    cityName.textContent = cityInput;
+    cityWeather.append(cityName);
+
+    console.log(cityName);
+
+    let cityDate = document.createElement("h1");
+    cityDate.textContent = today;
+    cityWeather.append(cityDate);
+
+    console.log(cityDate);
+
+
+    //display weather icon
+    // var iconPic = document.createElement("img").attributes;
+    //         "src"
+    //         "https://openweathermap.org/img/wn/" + icon + "@2x.png")
+        
+    // cityWeather.append(iconPic);
+
+    let cityTemp = document.createElement("h3");
+    cityTemp.textContent = ("Temp: " + temp + " °F");
+    cityWeather.append(cityTemp);
+
+    let cityWind = document.createElement("h3");
+    cityWind.textContent = ("Wind " + wind + " mph");
+    cityWeather.append(cityWind);
+
+    let cityHumidity = document.createElement("h3");
+    cityHumidity.textContent = ("Humidity: " + humidity);
+    cityWeather.append(cityHumidity);
+
+    localStorage.setItem("City", cityInput);
+
+    // const weathercard = 
+
+    for (i = 1; i < 6; i++) {
+        console.log(weatherArr[i]);
+    }
+
+    console.log(temp);
+    console.log(wind);
+    console.log(humidity);
+    console.log(icon);
 };
 
 
 
+    
 
-searchBtn.addEventListener('click', function() {
+// function savedSearches() {
+//     let searched = localStorage.getItem("city");
+
+//     if (searched === "") {
+//         userInput.value = "";
+//     } else {
+//         userInput.value = searched;
+//     }
+
+//     searchHistory.textContent = userInput.value;
+
+// }
+
+
+
+searchBtn.addEventListener('click', function () {
 
 
     if (userInput.value === "") {
         alert("Please enter a city")
     } else {
         localStorage.setItem('City', userInput.value);
+
     }
 
 
