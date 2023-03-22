85d170f0f13dbafcc52b1ef86dae7afc48eb6a22
ezmaa <ezinmark@gmail.com>
2023-03-21 21:30:51 -0400

    using fetch method instead of ajax

--- assets/script.js
+++ assets/script.js
@@ -1,162 +1,130 @@
 const apiKey = "d1e2d0763204896fd894698f5c6e27ee";
-const today = moment().format('L');
+const today = moment().format("L");
 let weatherArr = [];
 
-// User eneters name of city and gets the coordinates for that city 
 function currentCondition(city) {
+  const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-    const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  fetch(queryURL)
+    .then((response) => response.json())
+    .then((cityWeatherResponse) => {
+      $("#weatherContent").css("display", "block");
+      $("#cityDetail").empty();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(cityWeatherResponse) {
-        console.log(cityWeatherResponse);
-        
-        $("#weatherContent").css("display", "block");
-        $("#cityDetail").empty();
-        
-        const iconCode = cityWeatherResponse.weather[0].icon;
-        const iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+      const iconCode = cityWeatherResponse.weather[0].icon;
+      const iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
 
-        // WHEN I view current weather conditions for that city
-        // THEN I am presented with the city name
-        // the date
-        // an icon representation of weather conditions
-        // the temperature
-        // the humidity
-        // the wind speed
-        const currentCity = $(`
-            <h2 id="currentCity">
-                ${cityWeatherResponse.name} ${today} <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
-            </h2>
-            <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
-            <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
-            <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
+      const currentCity = $(`
+      <h2 id="currentCity">
+        ${cityWeatherResponse.name} ${today} <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
+      </h2>
+      <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
+      <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
+      <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
+    `);
+
+      $("#cityDetail").append(currentCity);
+
+      const lat = cityWeatherResponse.coord.lat;
+      const lon = cityWeatherResponse.coord.lon;
+      const uviQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+      fetch(uviQueryURL)
+        .then((response) => response.json())
+        .then((uviResponse) => {
+          const uvIndex = uviResponse.value;
+          const uvIndexP = $(`
+          <p>UV Index: 
+            <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
+          </p>
         `);
 
-        $("#cityDetail").append(currentCity);
+          $("#cityDetail").append(uvIndexP);
 
-        // UV index
-        const lat = cityWeatherResponse.coord.lat;
-        const lon = cityWeatherResponse.coord.lon;
-        const uviQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+          futureCondition(lat, lon);
 
-        $.ajax({
-            url: uviQueryURL,
-            method: "GET"
-        }).then(function(uviResponse) {
-            console.log(uviResponse);
-
-            const uvIndex = uviResponse.value;
-            const uvIndexP = $(`
-                <p>UV Index: 
-                    <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
-                </p>
-            `);
-
-            $("#cityDetail").append(uvIndexP);
-
-            futureCondition(lat, lon);
-
-            // WHEN I view the UV index
-            // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-            // 0-2 green#3EA72D, 3-5 yellow#FFF300, 6-7 orange#F18B00, 8-10 red#E53210, 11+violet#B567A4
-            if (uvIndex >= 0 && uvIndex <= 2) {
-                $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
-            } else if (uvIndex >= 3 && uvIndex <= 5) {
-                $("#uvIndexColor").css("background-color", "#FFF300");
-            } else if (uvIndex >= 6 && uvIndex <= 7) {
-                $("#uvIndexColor").css("background-color", "#F18B00");
-            } else if (uvIndex >= 8 && uvIndex <= 10) {
-                $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
-            } else {
-                $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white"); 
-            };  
+          if (uvIndex >= 0 && uvIndex <= 2) {
+            $("#uvIndexColor")
+              .css("background-color", "#3EA72D")
+              .css("color", "white");
+          } else if (uvIndex >= 3 && uvIndex <= 5) {
+            $("#uvIndexColor").css("background-color", "#FFF300");
+          } else if (uvIndex >= 6 && uvIndex <= 7) {
+            $("#uvIndexColor").css("background-color", "#F18B00");
+          } else if (uvIndex >= 8 && uvIndex <= 10) {
+            $("#uvIndexColor")
+              .css("background-color", "#E53210")
+              .css("color", "white");
+          } else {
+            $("#uvIndexColor")
+              .css("background-color", "#B567A4")
+              .css("color", "white");
+          }
         });
     });
 }
 
-
-// function to display the future condition 
 function futureCondition(lat, lon) {
+  const futureURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
 
-    //User is presented with a 5-day forecast
-    const futureURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
-
-    $.ajax({
-        url: futureURL,
-        method: "GET"
-    }).then(function(futureResponse) {
-        console.log(futureResponse);
-        $("#fiveDay").empty();
-        
-        for (let i = 1; i < 6; i++) {
-            const cityInfo = {
-                date: futureResponse.daily[i].dt,
-                icon: futureResponse.daily[i].weather[0].icon,
-                temp: futureResponse.daily[i].temp.day,
-                humidity: futureResponse.daily[i].humidity
-            };
-
-            const currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
-            const iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
-
-            // displays the date
-            // an icon representation of weather conditions
-            // the temperature
-            // the humidity
-            const futureCard = $(`
-                <div class="pl-3">
-                    <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem;>
-                        <div class="card-body">
-                            <h5>${currDate}</h5>
-                            <p>${iconURL}</p>
-                            <p>Temp: ${cityInfo.temp} °F</p>
-                            <p>Humidity: ${cityInfo.humidity}\%</p>
-                        </div>
-                    </div>
-                <div>
-            `);
-
-            $("#fiveDay").append(futureCard);
-        }
-    }); 
+  fetch(futureURL)
+    .then((response) => response.json())
+    .then((futureResponse) => {
+      $("#fiveDay").empty();
+      for (let i = 1; i < 6; i++) {
+        const cityInfo = {
+          date: futureResponse.daily[i].dt,
+          icon: futureResponse.daily[i].weather[0].icon,
+          temp: futureResponse.daily[i].temp.day,
+          humidity: futureResponse.daily[i].humidity,
+        };
+        const currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
+        const iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
+        const futureCard = $(`
+          <div class="pl-3">
+            <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem;>
+              <div class="card-body">
+                <h5>${currDate}</h5>
+                <p>${iconURL}</p>
+                <p>Temp: ${cityInfo.temp} °F</p>
+                <p>Humidity: ${cityInfo.humidity}\%</p>
+              </div>
+            </div>
+          </div>
+        `);
+        $("#fiveDay").append(futureCard);
+      }
+    });
 }
 
-// add on click event listener 
-$("#searchBtn").on("click", function(event) {
-    event.preventDefault();
-// val and trim methods takes in the user input and deletes uneccessary spaces 
-    const city = $("#enterCity").val().trim();
-    currentCondition(city);
-    if (!weatherArr.includes(city)) {
-        weatherArr.push(city);
-        const searchedCity = $(`
+$("#searchBtn").on("click", function (event) {
+  event.preventDefault();
+
+  const city = $("#enterCity").val().trim();
+  currentCondition(city);
+  if (!weatherArr.includes(city)) {
+    weatherArr.push(city);
+    const searchedCity = $(`
             <li class="list-group-item">${city}</li>
             `);
-        $("#searchHistory").append(searchedCity);
-    };
-    
-    localStorage.setItem("city", JSON.stringify(weatherArr));
-    console.log(weatherArr);
+    $("#searchHistory").append(searchedCity);
+  }
+
+  localStorage.setItem("city", JSON.stringify(weatherArr));
+  console.log(weatherArr);
 });
 
-// When the user clicks oon the saved city, the info for that city is displayed
-$(document).on("click", ".list-group-item", function() {
-    const listCity = $(this).text();
-    currentCondition(listCity);
+$(document).on("click", ".list-group-item", function () {
+  const listCity = $(this).text();
+  currentCondition(listCity);
 });
 
-// The user opens the weather app and the last city is displayed
-// searchHistoryArr gets the info for last saved item in local storage
-$(document).ready(function() {
-    const searchHistoryArr = JSON.parse(localStorage.getItem("city"));
+$(document).ready(function () {
+  const searchHistoryArr = JSON.parse(localStorage.getItem("city"));
 
-    if (searchHistoryArr !== null) {
-        const lastSearchedIndex = searchHistoryArr.length - 1;
-        const lastSearchedCity = searchHistoryArr[lastSearchedIndex];
-        currentCondition(lastSearchedCity);
-        console.log(`Last searched city: ${lastSearchedCity}`);
-    }
+  if (searchHistoryArr !== null) {
+    const lastSearchedIndex = searchHistoryArr.length - 1;
+    const lastSearchedCity = searchHistoryArr[lastSearchedIndex];
+    currentCondition(lastSearchedCity);
+  }
 });
